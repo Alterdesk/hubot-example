537a9cb513877ba6410ee18ac178209892f85ef6
jurriaan <jurriaan@alterdesk.com>
2018-03-07 09:50:36 +0100

    Able to generate, download and upload pdf

--- alterdesk-example-script/example.js
+++ alterdesk-example-script/example.js
@@ -13,6 +13,7 @@
 //   hubot help                 - Print help of this bot
 //   hubot command              - An example command
 //   hubot photo                - Upload a photo in the current chat
+//   hubot pdf                  - Generate and upload a pdf in the current chat
 //   hubot ping                 - Ping the bot
 //
 // Author:
@@ -28,6 +29,9 @@ var messengerApi;
 
 // Questionnaire control instance
 var control;
+
+// Script configurations
+var DELAY_PDF_CREATION_MS = 2000;
 
 module.exports = function(robot) {
 
@@ -47,7 +51,7 @@ module.exports = function(robot) {
     // Override the help regex to detect "what" and "support" in addition to "help"
     control.setHelpRegex(/help|what|support/, 'i');
     // Set the text to send when help was requested
-    control.setCatchHelpText("You can send \"command\" to start the questionnaire.");
+    control.setCatchHelpText("You can send \"command\" to start a questionnaire, \"photo\" to request a photo, \"pdf\" to request a pdf and \"ping\" to ping.");
 
     // Wait for two minutes for a reply from a user (overrides environment variable)
     control.setResponseTimeoutMs(120000);
@@ -60,7 +64,7 @@ module.exports = function(robot) {
     control.setCatchAllText("I did not understand what you said, type \"help\" to see what I can do for you.");
 
     // Mark the words "command" and "ping" as an accepted commands
-    control.addAcceptedCommands(["command", "photo", "ping"]);
+    control.addAcceptedCommands(["command", "photo", "pdf", "ping"]);
 
     // Override the default robot message receiver
     control.overrideReceiver(robot);
@@ -86,13 +90,23 @@ module.exports = function(robot) {
         });
     }),
 
+    // Check if the photo command was heard
     robot.hear(/photo/i, function(msg) {
+        // Create a message data instance
         var messageData = new Messenger.SendMessageData();
-        messageData.chatId = msg.message.room;
+
+        // Text body of the message
         messageData.message = "Here is the photo you requested";
+        // Optional attachment to send(local absolute or relative paths)
         messageData.addAttachmentPath("../bot.png");
+        // Chat id to send the message in
+        messageData.chatId = msg.message.room;
+        // If the chat is a group chat or a one-to-one chat
         messageData.isGroup = control.isUserInGroup(msg.message.user);
+        // Optional flag if chat auxiliary
         messageData.isAux = false;
+
+        // Send the message and parse result in callback
         messengerApi.sendMessage(messageData, function(success, json) {
             console.log("Send photo successful: " + success);
             if(json != null) {
@@ -102,6 +116,67 @@ module.exports = function(robot) {
                 msg.send("Was unable to upload photo to chat");
             }
         });
+    }),
+
+    // Check if the pdf command was heard
+    robot.hear(/pdf/i, function(msg) {
+        msg.send("Generating PDF, one moment please")
+
+        // Delay PDF creation to include latest messages
+        setTimeout(function() {
+            // Create a PDF data instance
+            var pdfData = new Messenger.PdfData();
+
+            // Chat id to generate pdf from
+            pdfData.chatId = msg.message.room;
+            // If the chat is a group chat or a one-to-one chat
+            pdfData.isGroup = control.isUserInGroup(msg.message.user);
+            // Optional flag if chat auxiliary
+            pdfData.isAux = false;
+            // Starting date of generated pdf (null == last 30 days)
+            pdfData.startDate = null;
+            // Ending date of generated pdf (null == latest message)
+            pdfData.endDate = null;
+            // Filename of the downloaded pdf
+            pdfData.name = "Chat.pdf";
+
+            // Retrieve chat pdf download url
+            messengerApi.getChatPdfUrl(pdfData, function(success, json, cookie) {
+                console.log("Retrieve pdf download url successful: " + success);
+                if(json != null) {
+                    var url = json["link"];
+
+                    // Use url and cookie to download pdf
+                    messengerApi.download(url, pdfData.name, "application/pdf", cookie, function(downloaded, path) {
+                        if(downloaded) {
+                            console.log("pdf: Path: " + path);
+                            pdfData.path = path;
+
+                            // Upload the downloaded pdf file to the chat
+                            var messageData = new Messenger.SendMessageData();
+                            messageData.chatId = pdfData.chatId;
+                            messageData.message = "Here is the PDF you requested";
+                            messageData.addAttachmentPath(pdfData.path);
+                            messageData.isGroup = pdfData.isGroup;
+                            messageData.isAux = pdfData.isAux;
+                            messengerApi.sendMessage(messageData, function(success, json) {
+                                console.log("Send pdf successful: " + success);
+                                if(json != null) {
+                                    var messageId = json["id"];
+                                    console.log("PDF message id: " + messageId);
+                                } else {
+                                    msg.send("Was unable to upload PDF to chat");
+                                }
+                            });
+                        } else {
+                            msg.send("Was unable to download PDF file");
+                        }
+                    });
+                } else {
+                    msg.send("Was unable to retrieve PDF file");
+                }
+            });
+        }, DELAY_PDF_CREATION_MS);
     }),
 
     // Example simple command that does not use the questionnaire
